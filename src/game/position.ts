06594859976bf5323c87fd5fd6f3e3f2c2cc6c2f
Maverick Zhang <maverickzhang@berkeley.edu>
2021-03-11 23:15:04 -0500

    Added a nicer Positions default creator

--- src/game/position.ts
+++ src/game/position.ts
@@ -6,21 +6,26 @@ import { BitBoard } from "./bitboard";
 export const FILES: number = 9;
 export const RANKS: number = 10;
 
-export const G_R: bigint = BigInt(1 << 4);
-export const A_R: bigint = BigInt(1 << 3) + BigInt(1 << 5);
-export const E_R: bigint = BigInt(1 << 2) + BigInt(1 << 6);
-export const H_R: bigint = BigInt(1 << 1) + BigInt(1 << 7);
-export const R_R: bigint = BigInt(1 << 0) + BigInt(1 << 9);
-export const C_R: bigint = BigInt(1 << 19) + BigInt(1 << 25);
-export const P_R: bigint = BigInt(1 << 27) + BigInt(1 << 29) + BigInt(1 << 31) + BigInt(1 << 33) + BigInt(1 << 35);
+export const G_R: number[] = [4];
+export const A_R: number[] = [3, 5];
+export const E_R: number[] = [2, 6];
+export const H_R: number[] = [1, 7];
+export const R_R: number[] = [0, 9];
+export const C_R: number[] = [19, 25];
+export const P_R: number[] = [27, 29, 31, 33, 35];
 
-export const G_B: bigint = BigInt(1 << 85);
-export const A_B: bigint = BigInt(1 << 84) + BigInt(1 << 86);
-export const E_B: bigint = BigInt(1 << 83) + BigInt(1 << 87);
-export const H_B: bigint = BigInt(1 << 82) + BigInt(1 << 88);
-export const R_B: bigint = BigInt(1 << 81) + BigInt(1 << 89);
-export const C_B: bigint = BigInt(1 << 64) + BigInt(1 << 70);
-export const P_B: bigint = BigInt(1 << 54) + BigInt(1 << 56) + BigInt(1 << 58) + BigInt(1 << 60) + BigInt(1 << 62);
+export const G_B: number[] = [85];
+export const A_B: number[] = [84, 86];
+export const E_B: number[] = [83, 87];
+export const H_B: number[] = [82, 88];
+export const R_B: number[] = [81, 89];
+export const C_B: number[] = [64, 70];
+export const P_B: number[] = [54, 56, 58, 60, 62];
+
+export const INDICES: number[][] = [G_R, A_R, E_R, H_R, R_R, C_R, P_R, G_B, A_B, E_B, H_B, R_B, C_B, P_B];
+
+const one: bigint = BigInt(1);
+const zero: bigint = BigInt(0);
 
 
 export class Position {
@@ -33,25 +38,20 @@ export class Position {
         });
     }
 
-    static start() {
-        var positions: BitBoard[] = [];
-        positions.push(new BitBoard(G_R));
-        positions.push(new BitBoard(A_R));
-        positions.push(new BitBoard(E_R));
-        positions.push(new BitBoard(H_R));
-        positions.push(new BitBoard(R_R));
-        positions.push(new BitBoard(C_R));
-        positions.push(new BitBoard(P_R));
+    private static makeBitBoards() {
+        var boards: BitBoard[] = []  
+        INDICES.forEach(piece => {
+            var b = new BitBoard(zero);
+            piece.forEach(i => {
+                b.set(i, true);
+            });
+            boards.push(b);
+        });
+        return boards;
+    }
 
-        positions.push(new BitBoard(G_B));
-        positions.push(new BitBoard(A_B));
-        positions.push(new BitBoard(E_B));
-        positions.push(new BitBoard(H_B));
-        positions.push(new BitBoard(R_B));
-        positions.push(new BitBoard(C_B));
-        positions.push(new BitBoard(P_B));
-        
-        return new Position(positions);
+    static start() {
+        return new Position(this.makeBitBoards());
     }
 
     copy() {
